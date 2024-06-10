b8b0f9b6889b097db5b72b8e5f99deecdbcc8a9b
Andrew Nowak <andrew.nowak@guardian.co.uk>
2024-06-10 11:58:21 +0100

    remove bean dependency

--- src/javascripts/crosswords/clues.js
+++ src/javascripts/crosswords/clues.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line max-classes-per-file
 import React, { Component, createRef } from 'react';
-import bean from 'bean';
 import fastdom from 'fastdom';
 import { classNames } from './classNames';
 import { isBreakpoint } from '../lib/detect';
@@ -54,8 +53,8 @@ class Clues extends Component {
 
     const height = this.$cluesNode.scrollHeight - this.$cluesNode.clientHeight;
 
-    bean.on(this.$cluesNode, 'scroll', (e) => {
-      const showGradient = height - e.currentTarget.scrollTop > 25;
+    this.$cluesNode.addEventListener('scroll', (event) => {
+      const showGradient = height - event.currentTarget.scrollTop > 25;
 
       if (this.state.showGradient !== showGradient) {
         this.setState({
